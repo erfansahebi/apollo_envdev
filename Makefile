6efcdbb387d9998706a968dd75b24110ce2a90fa
Erfan Sahebi <erfansahebivayghan@gmail.com>
2023-11-04 08:02:43 +0330

    update

--- Makefile
+++ Makefile
@@ -2,7 +2,6 @@ SHELL := /bin/bash
 IMAGES := apollo_auth \
           apollo_shared \
           apollo_gateway \
-          apollo_nginx
-
+		  apollo_rss
 build:
 	for image in $(IMAGES) ; do make -C $$image build-image; done
--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 base_repo_url="https://github.com/erfansahebi/apollo"
 
-declare -a repos=("shared" "auth" "gateway" "nginx")
+declare -a repos=("shared" "auth" "gateway" "rss")
 
 for repo in "${repos[@]}"; do
   echo "----- Cloning ${repo} -----"
